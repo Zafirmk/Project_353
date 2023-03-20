610536571e9fdce053b0bc761db6aad4cfe6b5f9
MarwaKhalid <khalidmarwa786@gmail.com>
2023-03-20 15:17:08 -0400

    Finished delete row function

--- HFESTS/Database/delete.php
+++ HFESTS/Database/delete.php
@@ -1,47 +1,32 @@
   <?php
+  include_once 'config.php';
+
   if ($_GET['table_name'] == 'Employees_Managers') {
-    echo $_GET["EID"];
-    echo $_GET["FN"];
-    echo $_GET["LN"];
-    echo $_GET["DOB"];
-    echo $_GET["MCN"];
-    echo $_GET["Tel"];
-    echo $_GET["Add"];
-    echo $_GET["City"];
-    echo $_GET["Prov"];
-    echo $_GET["PC"];
-    echo $_GET["CS"];
-    echo $_GET["EA"];
-    echo $_GET["Role"];
-    echo $_GET["IM"];
+    $sql = "DELETE FROM Employees_Managers WHERE EmployeeID='{$_GET["EID"]}' AND FirstName='{$_GET['FN']}' AND LastName='{$_GET["LN"]}' AND DateOfBirth='{$_GET["DOB"]}' AND MedicareCardNumber='{$_GET["MCN"]}' AND TelephoneNumber='{$_GET["Tel"]}' AND Address='{$_GET["Add"]}' AND City='{$_GET["City"]}' AND Province='{$_GET["Prov"]}' AND PostalCode='{$_GET["PC"]}' AND Citizenship='{$_GET["CS"]}' AND EmailAddress='{$_GET["EA"]}' AND Role='{$_GET["Role"]}' AND Is_Manager='{$_GET["IM"]}'";
+    $URLpath = "../Employees_Managers/E_Delete.php";
   } else if ($_GET['table_name'] == 'Facility') {
-    echo $_GET["FID"];
-    echo $_GET["Name"];
-    echo $_GET["Add"];
-    echo $_GET["City"];
-    echo $_GET["Prov"];
-    echo $_GET["PC"];
-    echo $_GET["PN"];
-    echo $_GET["WA"];
-    echo $_GET["Type"];
-    echo $_GET["Cap"];
+    $sql = "DELETE FROM Facility WHERE FacilityID='{$_GET["FID"]}' AND Name='{$_GET["Name"]}' AND Address='{$_GET["Add"]}' AND City='{$_GET["City"]}' AND Province='{$_GET["Prov"]}' AND PostalCode='{$_GET["PC"]}' AND PhoneNumber='{$_GET["PN"]}' AND WebAddress='{$_GET["WA"]}' AND Type='{$_GET["Type"]}' AND Capacity='{$_GET["Cap"]}'";
+    $URLpath = "../Facility/F_Delete.php";
   } else if ($_GET['table_name'] == 'Vaccination') {
-    echo $_GET["EID"];
-    echo $_GET["FID"];
-    echo $_GET["Type"];
-    echo $_GET["DN"];
-    echo $_GET["Date"];
+    $sql = "DELETE FROM Vaccination WHERE EmployeeID='{$_GET["EID"]}' AND FacilityID='{$_GET["FID"]}' AND VaccineType='{$_GET["Type"]}' AND DoseNumber='{$_GET["DN"]}' AND VaccinationDate='{$_GET["Date"]}'";
+    $URLpath = "../Vaccination/V_Delete.php";
   } else if ($_GET['table_name'] == 'Infection') {
-    echo $_GET["EID"];
-    echo $_GET["Name"];
-    echo $_GET["Type"];
-    echo $_GET["Date"];
+    $sql = "DELETE FROM Infection WHERE EmployeeID='{$_GET["EID"]}' AND InfectionName='{$_GET["Name"]}' AND InfectionType='{$_GET["Type"]}' AND InfectionDate='{$_GET["Date"]}'";
+    $URLpath = "../Infection/I_Delete.php";
   } else if ($_GET['table_name'] == 'Schedule') {
-    echo $_GET["EID"];
-    echo $_GET["FID"];
-    echo $_GET["SDate"];
-    echo $_GET["EDate"];
-    echo $_GET["STime"];
-    echo $_GET["ETime"];
+    $sql = "DELETE FROM Infection WHERE EmployeeID='{$_GET["EID"]}' AND FacilityID='{$_GET["FID"]}' AND StartDate='{$_GET["SDate"]}' AND EndDate='{$_GET["EDate"]}' AND StartTime='{$_GET["STime"]}' AND EndTime='{$_GET["ETime"]}'";
+    $URLpath = "../Schedule/S_Delete.php";
+  }
+
+  if (mysqli_query($conn, $sql)) {
+    echo "<script language='javascript'>";
+    echo 'alert("Record deleted successfully");';
+    echo 'window.location.replace("' . $URLpath . '");';
+    echo "</script>";
+  } else {
+    $message = "ERROR: " . mysqli_error($conn);
+    echo "<script type='text/javascript'>alert('$message');";
+    echo 'window.location.replace("' . $URLpath . '");';
+    echo "</script>";
   }
   ?>
