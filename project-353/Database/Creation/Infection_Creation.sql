20efe8b5c4e0acfb4c233c26662a73acc35486fa
Asha Islam <85359230+asha97@users.noreply.github.com>
2023-04-02 22:52:45 -0400

    Delete project-353 directory

--- project-353/Database/Creation/Infection_Creation.sql
+++
@@ -1,9 +0,0 @@
-USE fac353_4;
-CREATE TABLE Infection (
-    EmployeeID INT,
-    InfectionName VARCHAR(100),
-    InfectionType VARCHAR(50),
-    InfectionDate DATE,
-    PRIMARY KEY (EmployeeID, InfectionDate),
-    FOREIGN KEY (EmployeeID) REFERENCES Employees_Managers(EmployeeID)
-);
