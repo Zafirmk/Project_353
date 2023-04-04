6f6e126927bd933a6eb6a8a98759e4c3c9f5cfb6
Zafirmk <zafirmk0@gmail.com>
2023-04-03 21:20:40 -0400

    Actual bug fix

--- HFESTS/Database/email_every_sunday.php
+++ HFESTS/Database/email_every_sunday.php
@@ -90,13 +90,13 @@
     $mail->Username = "compd9376@gmail.com";
     $mail->Password = "eucmgghkonheltbn";
     $mail->Subject = $subject;
-    $mail->setFrom("compd9376@gmail.com", $From);
+    $mail->setFrom("compd9376@gmail.com", $from);
     $mail->Body = $emailbody;
     $mail->addAddress($to);
   
     if ($mail->Send()) {
       echo "Email sent";
-      $email_query = "INSERT INTO Emails VALUES ('" . date("Y-m-d") . "', '" . $From . "', '" . $mail->Subject . "', '" . $mail->Body . "');";
+      $email_query = "INSERT INTO Emails VALUES ('" . date("Y-m-d") . "', '" . $from . "', '" . $mail->Subject . "', '" . $mail->Body . "');";
       if (mysqli_query($conn, $email_query)) {
         echo "New record created successfully";
       } else {
